c0854f326fe875d4d54a5818f52790b520debf44
lisaidy <lisaidy@163.com>
2026-07-23 18:01:52 +0800

    Initialize Vulkan instance and device

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -7,6 +7,26 @@
 
 class VulkanEngine {
 public:
+	VkInstance _instance;
+	VkDebugUtilsMessengerEXT _debug_messenger;
+	VkPhysicalDevice _chosenGPU;
+	VkDevice _device;
+	VkSurfaceKHR _surface;
+
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
+private:
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
+public:
 
 	bool _isInitialized{ false };
 	int _frameNumber {0};
